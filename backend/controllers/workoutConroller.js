8b0805624a14136d251b5ed0dec2d09895501c5b
nlnl03 <lavinoam2003@gmail.com>
2024-03-03 01:19:46 +0200

    NEW

---
+++ backend/controllers/workoutConroller.js
@@ -0,0 +1,31 @@
+const Workout = require("../models/WorkoutModel");
+
+//get all workouts
+const getWorkouts = async (req, res) => {
+  const workouts = await Workout.find({}).sort({ createdAt: -1 });
+
+  res.status(200).json(workouts);
+};
+
+//get a single
+
+//create a new
+const createWorkout = async (req, res) => {
+  const { title, load, reps } = req.body;
+
+  //add doc to db
+  try {
+    const workout = await Workout.create({ title, load, reps });
+    res.status(200).json(workout);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
+//delete
+
+//update
+
+module.exports = {
+  createWorkout,
+};
---
+++ backend/routes/workouts.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const { createWorkout } = require("../controllers/workoutConroller");
+const router = express.Router();
+
+//get all workouts
+router.get("/", (req, res) => {
+  res.json({ mssg: "get all workouts" });
+});
+
+//get a single workout
+router.get("/:id", (req, res) => {
+  res.json({ mssg: "get a single workout" });
+});
+
+//post a new workout
+router.post("/", createWorkout);
+
+//delete a workout
+router.delete("/:id", (req, res) => {
+  res.json({ mssg: "delete a workout" });
+});
+
+//update a workout
+router.patch("/:id", (req, res) => {
+  res.json({ mssg: "update a workout" });
+});
+
+module.exports = router;
